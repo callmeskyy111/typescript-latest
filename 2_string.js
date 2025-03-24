ac9640d49e9f3dec5a9b066c2d94a7a0c132738a
Soumadip <39thdecember@gmail.com>
2025-03-24 22:33:22 +0530

    Str and Bool types ☑️

---
+++ 2_string.js
@@ -0,0 +1,17 @@
+//string & boolean data-types
+var str = "Soumadip Banerjee";
+var str2 = "Hello how are you?";
+var userName = "Skyy";
+var userAge = 29;
+var info = "My name is ".concat(userName, " and age is ").concat(userAge, ".");
+console.log(info);
+//num -> str. conversion (toString())
+var num1 = 10;
+var numStr = num1.toString();
+console.log(numStr);
+// Bool
+var boolData = true;
+var boolStr = boolData.toString();
+console.log(boolData);
+console.log(boolStr);
+var randStr = "Some str."; //inference
